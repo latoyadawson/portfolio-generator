ed1c01d63354cac9f182736b8327d4e3e4bc3c84
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-04-05 19:38:46 -0700

    ask for name

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+const inquirer = require("inquirer");
+// //requires a file system 
+// const fs = require('fs');
+// const generatePage = require("./src/page-template.js");
 
-//requires a file system 
-const fs = require('fs');
-const generatePage = require("./src/page-template.js");
-//holds user command line arguments 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name,github)
 
-const [name, github] = profileDataArgs;
+// //fs files require three arguments name of file, data, and callback function
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
 
-const pageHTML = generatePage(name,github)
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-//fs files require three arguments name of file, data, and callback function
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
