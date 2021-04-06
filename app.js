8cb101c5a49a01fc263244116807c063867d937e
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-04-06 14:09:15 -0700

    add multiple projects to index

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 
-// //requires a file system 
-// const fs = require('fs');
-// const generatePage = require("./src/page-template.js");
+//requires a file system 
+const fs = require('fs');
+const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(name,github)
 
@@ -146,6 +146,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData)
+
+        //fs files require three arguments name of file, data, and callback function
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
+        // console.log(portfolioData);
     });
     
